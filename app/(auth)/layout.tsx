d9cfa6e95394f5c1a0b05f04139c5c073a68c14b
jmg <jmg41490400@gmail.com>
2023-09-23 15:23:15 +0900

    feat: HomePage

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 export const metadata = {
   title: "Threads",
-  description: "A Next.js 13 Meta Threads Application",
+  description: "Meta Threads Application",
 };
 
 const inter = Inter({ subsets: ["latin"] });
