4ffce94c7ecaa80e02b563f906b5cc52dd052aa4
jmg <jmg41490400@gmail.com>
2023-09-24 14:27:53 +0900

    feat: Activity page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,15 +7,13 @@ export const metadata = {
 };
 
 const inter = Inter({ subsets: ["latin"] });
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang={"en"}>
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className={"w-full flex justify-center items-center min-h-screen"}>{children}</div>
+        </body>
       </html>
     </ClerkProvider>
   );
--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { currentUser } from "@clerk/nextjs";
+import { fetchUser, getActivity } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  // getActivity
+  const activity = await getActivity(userInfo._id);
   return (
-    <section>
-      <div className={"text-light-2"}>Activity Page</div>
-    </section>
+    <>
+      <h1 className="head-text">Activity</h1>
+
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt="user_logo"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">{activity.author.name}</span> replied to
+                    your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
+    </>
   );
 };
 
